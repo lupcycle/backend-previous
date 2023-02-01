45b0df46bbc3026fb67b3d2e1b195d8941f4bfdb
cypark <cop0817@naver.com>
2023-02-01 15:21:22 +0900

    프로젝트 시작

---
+++ src/test/java/com/simin/lupcycle/LupcycleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simin.lupcycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupcycleeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
